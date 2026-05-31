0d7338b95a9dd7d26cd0223a7a9e45ca39641ced
emad-aisha <emadaisha2024@gmail.com>
2026-05-31 09:47:02 +0300

    Player Movement

---
+++ Assets/2 - Scripts/Player/CameraController.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField] int maxY;
+    [SerializeField] int minY;
+
+    [SerializeField] int sensitivity;
+
+    [SerializeField] bool invertY;
+    [SerializeField] bool invertX;
+
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+}
